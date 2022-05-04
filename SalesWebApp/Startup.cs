0680edf04d0841b27b1b0c50e3a9d1d779c747ff
Scarzin <sc4rzin@gmail.com>
2022-05-04 19:16:58 -0300

    CRUD scaffolding test

--- SalesWebApp/Startup.cs
+++ SalesWebApp/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebApp.Models;
 
 namespace SalesWebApp
 {
@@ -24,6 +26,9 @@ namespace SalesWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SalesWebAppContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
